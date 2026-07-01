4c0d5e484c44f22613861ab3ecd5b6418d65e47e
Tyshavis Jones <wicklurkin@Tyshaviss-Air.lan>
2026-07-01 02:36:11 -0400

    Fix launch page

---
+++ app/launch/page.tsx
@@ -0,0 +1,7 @@
+export default function LaunchPage() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl">Launch Page</h1>
+    </main>
+  );
+}
